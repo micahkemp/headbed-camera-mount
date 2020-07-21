e06676067278540c0b10cfef6f703ecfc9fb1a28
Micah Kemp <apple@micah.ws>
2020-07-20 21:33:38 -0500

    Moved spacer to its own class.

--- bedmount.py
+++ bedmount.py
@@ -1,53 +1,42 @@
 from scadder import Union, Cube, Cylinder, Difference, Translate, XYZ
 
 
-class BedMount(Union):
-    spacer_diameter = 6
-    spacer_diameter_tolerance = 0.1
-    spacer_height = 5.5
-    spacer_offset = 12
-    guide_width = 12.3
-    guide_width_tolerance = 0.1
+class BedMountSpacerComponent(Union):
+    def __init__(self, height, hole_offset, hole_diameter):
+        super(BedMountSpacerComponent, self).__init__(name=None, children=None)
 
-    def __init__(self, name=None):
-        super(BedMount, self).__init__(name=name, children=None)
+        self.height = height
+        self.hole_offset = hole_offset
+        self.hole_diameter = hole_diameter
 
         self.add_children([
             self.spacer_component(),
         ])
 
     @property
-    def spacer_cube_length(self):
-        return self.spacer_offset * 2
+    def length(self):
+        return self.hole_offset * 2
 
     @property
-    def spacer_cube_width(self):
-        return self.spacer_offset * 2
-
-    @property
-    def spacer_cube_height(self):
-        return self.spacer_height
-
-    @property
-    def spacer_hole_diameter(self):
-        return self.spacer_diameter + self.spacer_diameter_tolerance
+    def width(self):
+        return self.hole_offset * 2
 
     def spacer_cube(self):
         return Cube(
             name="spacer_cube",
-            length=self.spacer_cube_length,
-            width=self.spacer_cube_width,
-            height=self.spacer_height,
+            length=self.length,
+            width=self.width,
+            height=self.height,
         )
 
     def spacer_hole(self):
         return Translate(
             name="spacer_hole",
-            vector=XYZ(self.spacer_cube_length/2, self.spacer_cube_width/2, 0),
+            vector=XYZ(self.length/2, self.width/2, 0),
             children=[
                 Cylinder(
-                    diameter=self.spacer_hole_diameter,
-                    height=self.spacer_cube_height,
+                    diameter=self.hole_diameter,
+                    height=self.height,
                 )
             ]
         )
@@ -60,6 +49,22 @@ class BedMount(Union):
                 self.spacer_hole(),
             ]
         )
+
+class BedMount(Union):
+    spacer_diameter = 6.2
+    spacer_height = 5.5
+    spacer_offset = 12
+
+    def __init__(self, name=None):
+        super(BedMount, self).__init__(name=name, children=None)
+
+        self.add_children([
+            BedMountSpacerComponent(
+                height=self.spacer_height,
+                hole_offset=self.spacer_offset,
+                hole_diameter=self.spacer_diameter,
+            ),
+        ])
 
 
 if __name__ == "__main__":
