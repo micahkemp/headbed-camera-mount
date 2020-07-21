4268ed8c2c7a096b8efc1938d693ae59126de42c
Micah Kemp <apple@micah.ws>
2020-07-20 22:08:30 -0500

    Added guide.

--- bedmount.py
+++ bedmount.py
@@ -2,8 +2,8 @@ from scadder import Union, Cube, Cylinder, Difference, Translate, XYZ
 
 
 class BedMountSpacerComponent(Union):
-    def __init__(self, height, hole_offset, hole_diameter):
-        super(BedMountSpacerComponent, self).__init__(name=None, children=None)
+    def __init__(self, height, hole_offset, hole_diameter, name=None):
+        super(BedMountSpacerComponent, self).__init__(name=name, children=None)
 
         self.height = height
         self.hole_offset = hole_offset
@@ -50,19 +50,80 @@ class BedMountSpacerComponent(Union):
             ]
         )
 
+
+class BedMountGuideRailsComponent(Difference):
+    def __init__(self, guide_width, height, spacer_offset):
+        super(BedMountGuideRailsComponent, self).__init__(name=None, children=None)
+
+        self.guide_width = guide_width
+        self.height = height
+        self.spacer_offset = spacer_offset
+
+        self.add_children([
+            self.outer_cube(),
+            self.guide_cutout(),
+        ])
+
+    @property
+    def outer_cube_length(self):
+        return self.spacer_offset * 2
+
+    @property
+    def outer_cube_width(self):
+        return self.spacer_offset * 2
+
+    def outer_cube(self):
+        return Cube(
+            name="outer_cube",
+            length=self.outer_cube_length,
+            width=self.outer_cube_width,
+            height=self.height,
+        )
+
+    def guide_cutout(self):
+        return Translate(
+            name="guide_cutout",
+            vector=XYZ(
+                self.outer_cube_length / 2 - self.guide_width / 2,
+                0,
+                0,
+            ),
+            children=[
+                Cube(
+                    length=self.guide_width,
+                    width=self.outer_cube_width,
+                    height=self.height,
+                )
+            ]
+        )
+
+
 class BedMount(Union):
     spacer_diameter = 6.2
     spacer_height = 5.5
     spacer_offset = 12
+    guide_width = 12.3
 
     def __init__(self, name=None):
         super(BedMount, self).__init__(name=name, children=None)
 
         self.add_children([
             BedMountSpacerComponent(
+                name="spacer_layer",
                 height=self.spacer_height,
                 hole_offset=self.spacer_offset,
                 hole_diameter=self.spacer_diameter,
+            ),
+            Translate(
+                name="guide_layer",
+                vector=XYZ(0, 0, -self.spacer_height),
+                children=[
+                    BedMountGuideRailsComponent(
+                        guide_width=self.guide_width,
+                        height=self.spacer_height,
+                        spacer_offset=self.spacer_offset,
+                    ),
+                ]
             ),
         ])
 
